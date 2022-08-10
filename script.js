96e803b34ac5bcd47ce052434bf32a2ea70eccbd
Elenilson <elenilsonsantos9@gmail.com>
2022-08-09 22:47:05 -0300

    exercicios resolvidos

---
+++ script.js
@@ -0,0 +1,40 @@
+// const  bool1 = false
+// const  bool2 = true
+// const  bool3 = true
+// // if(bool1 === true){
+// // alert('entrou no if. Bool1 é true')
+// // }else{
+// //     alert('entrou no else. bool1 é false')
+// // }
+//  if(bool1 === bool2){
+//     alert('o valor de bool1 e bool2 são iguais')
+//  }else if(bool2 === bool3){
+//     alert('o valor de boo2 e bool3 são iguais')
+//  }else if(bool1 ===bool3){
+//     alert('o valor de boll1 e bool3 são iguais')
+//  }else{
+//     alert('não existem valores iguais')
+//  }
+// let idade = 29
+
+
+// if(idade >= 60||idade >=16 && idade <= 17){
+//     console.log('titulo facultativo')
+// }else if(idade >= 18){
+//     console.log('pode tirar o tirar o titulo de eleitor')
+// }else{
+//     console.log('você não pode tirar o titulo de eleitor')
+// }
+
+// let   media = 4
+
+// if(media >= 5){
+// console.log('aprovado(a)')
+// }else if(media>=3){
+// console.log('voce esta de recuperação')
+// }else if(media<3){
+// console.log('voce foi reprovado')
+// }else{
+//     console.log('dados inválidos')
+// }
+
